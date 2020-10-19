fd03e5cb97940e9b8899a969cbae39d41ef671db
Alexander <alexxdaas@gmail.com>
2020-10-19 18:49:03 +0500

    make TestEnvironment database in memory

--- src/main/tests/TestEnvironment.java
+++ src/main/tests/TestEnvironment.java
@@ -4,7 +4,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class TestEnvironment {
-    public static DataBaseConnectionInfo dataBaseConnectionInfo = new DataBaseConnectionInfo("jdbc:h2:./Test", org.h2.Driver.load());
+    public static DataBaseConnectionInfo dataBaseConnectionInfo = new DataBaseConnectionInfo("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", org.h2.Driver.load());
     public static Injector CreateInjector()
     {
         return Guice.createInjector(new DIContainer(dataBaseConnectionInfo));
