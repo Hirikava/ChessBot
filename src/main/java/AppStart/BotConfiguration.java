adad21b27df878f516ad12e1194dfbc586160c81
Alexander <alexxdaas@gmail.com>
2020-11-05 14:02:19 +0500

    fix build

--- src/main/java/AppStart/BotConfiguration.java
+++ src/main/java/AppStart/BotConfiguration.java
@@ -3,12 +3,13 @@ package AppStart;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 
 public class BotConfiguration {
     public static void Configure(TelegramLongPollingBot bot) {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(bot);
         } catch (TelegramApiException e) {
             e.printStackTrace();
