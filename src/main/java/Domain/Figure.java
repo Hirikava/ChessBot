29ec8b31535351809009632701d2ce1a4b082685
Anastasia <anastasia_telegina@mail.ru>
2020-12-03 10:27:04 +0500

    ChessField

---
+++ src/main/java/Domain/Figure.java
@@ -0,0 +1,19 @@
+package Domain;
+
+public class Figure {
+    private Pieces FiguresName;
+    private PlayerColour colour;
+
+    public Figure(Pieces FiguresName, PlayerColour colour){
+        this.FiguresName = FiguresName;
+        this.colour = colour;
+    }
+
+    public Pieces getFiguresName(){
+        return FiguresName;
+    }
+
+    public PlayerColour getColour(){
+        return colour;
+    }
+}
--- src/main/java/Domain/GameSession.java
+++ src/main/java/Domain/GameSession.java
@@ -3,20 +3,31 @@ package Domain;
 import java.util.Optional;
 
 public class GameSession {
-    public TurnResult MakeTurn(PlayerColour colour, Coords coordsFrom, Coords coordsTo) {
-        if (colour != Turn)
-            return new TurnResult(TurnError.AnotherPlayerTurn, createGameState());
-        return new TurnResult(TurnError.None, createGameState());
-    }
+    private PlayerColour Turn;
 
     public GameSession() {
         Turn = PlayerColour.White;
     }
 
     private GameState createGameState() {
-        return new GameState(Optional.of(PlayerColour.Black));
+        return new GameState(Optional.of(Turn));
     }
 
-    private PlayerColour Turn;
+    /*public void FiguresTurn(ChessField field, Coords coordsFrom, Coords coordsTo){
+        Figure figure = ChessField.chessBoard[coordsFrom.X][coordsFrom.Y];
+        switch (figure.getFiguresName()){
+            case King:
+                //метод(цвет, координатыОткуда, координатыКуда)
+                break;
+            case Pawn:
+                break;
+        }
+    }*/
 
+
+    public TurnResult MakeTurn(PlayerColour colour, Coords coordsFrom, Coords coordsTo) {
+        if (colour != Turn)
+            return new TurnResult(TurnError.AnotherPlayerTurn, createGameState());
+        return new TurnResult(TurnError.None, createGameState());
+    }
 }
