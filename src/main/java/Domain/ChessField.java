bcbc7e006bc5789a48927d8e71eb060fba151023
Anastasia <anastasia_telegina@mail.ru>
2020-12-13 20:14:12 +0500

    game field

--- src/main/java/Domain/ChessField.java
+++ src/main/java/Domain/ChessField.java
@@ -3,40 +3,41 @@ package Domain;
 import ServerModels.Player;
 
 public class ChessField {
-   public Figure [][] chessBoard;
+   private Figure [][] chessBoard;
 
     ChessField(){
         chessBoard = new Figure[8][8];
         chessBoard[0][0] = new Figure(Pieces.Rook, PlayerColour.Black);
         chessBoard[0][1] = new Figure(Pieces.Knight, PlayerColour.Black);
         chessBoard[0][2] = new Figure(Pieces.Bishop, PlayerColour.Black);
-       /*
-        chessBoard[0][2] = new Bishop("black");
-        chessBoard[0][3] = new Queen("black");
-        chessBoard[0][4] = new King("black");
-        chessBoard[0][5] = new Bishop("black");
-        chessBoard[0][6] =  new Knight(("black"));
-        chessBoard[0][7] = new Rook("black");
+        chessBoard[0][3] = new Figure(Pieces.Queen, PlayerColour.Black);
+        chessBoard[0][4] = new Figure(Pieces.King, PlayerColour.Black);
+        chessBoard[0][5] = new Figure(Pieces.Bishop, PlayerColour.Black);
+        chessBoard[0][6] =  new Figure(Pieces.Knight, PlayerColour.Black);
+        chessBoard[0][7] = new Figure(Pieces.Rook, PlayerColour.Black);
 
         for (int i = 1;i<2; i++){
             for (int j = 0; j < 8; j++)
-                chessBoard[i][j] = new Pawn("black");
+                chessBoard[i][j] = new Figure(Pieces.Pawn, PlayerColour.Black);
         }
 
         for (int i = 6;i<7; i++){
             for (int j = 0; j < 8; j++)
-                chessBoard[i][j] = new Pawn("white");
+                chessBoard[i][j] = new Figure(Pieces.Pawn, PlayerColour.White);
         }
 
-        chessBoard[7][0] = new Rook("white");
-        chessBoard[7][1] = new Knight("white");
-        chessBoard[7][2] = new Bishop("white");
-        chessBoard[7][3] = new Queen("white");
-        chessBoard[7][4] = new King("white");
-        chessBoard[7][5] = new Bishop("white");
-        chessBoard[7][6] =  new Knight(("white"));
-        chessBoard[7][7] = new Rook("white");*/
 
+        chessBoard[7][0] = new Figure(Pieces.Rook, PlayerColour.White);
+        chessBoard[7][1] = new Figure(Pieces.Knight, PlayerColour.White);
+        chessBoard[7][2] = new Figure(Pieces.Bishop, PlayerColour.White);
+        chessBoard[7][3] = new Figure(Pieces.Queen, PlayerColour.White);
+        chessBoard[7][4] = new Figure(Pieces.King, PlayerColour.White);
+        chessBoard[7][5] = new Figure(Pieces.Bishop, PlayerColour.White);
+        chessBoard[7][6] =  new Figure(Pieces.Knight, PlayerColour.White);
+        chessBoard[7][7] = new Figure(Pieces.Rook, PlayerColour.White);
+    }
 
+    public Figure getFigure(Coords coords){
+        return chessBoard[coords.X][coords.Y];
     }
 }
--- src/main/java/Domain/GameSession.java
+++ src/main/java/Domain/GameSession.java
@@ -13,19 +13,44 @@ public class GameSession {
         return new GameState(Optional.of(Turn));
     }
 
-    /*public void FiguresTurn(ChessField field, Coords coordsFrom, Coords coordsTo){
-        Figure figure = ChessField.chessBoard[coordsFrom.X][coordsFrom.Y];
-        switch (figure.getFiguresName()){
-            case King:
-                //метод(цвет, координатыОткуда, координатыКуда)
-                break;
-            case Pawn:
-                break;
-        }
-    }*/
+    public Boolean PawnsTurn(ChessField field, Coords coordsFrom, Coords coordsTo){
+        return true;
+    }
 
+    public Boolean FiguresTurn(ChessField field, Coords coordsFrom, Coords coordsTo){
+        Figure figure = field.getFigure(coordsFrom);
+        if (figure == null) return false;
+        else
+            switch (figure.getFiguresName()){
+                case King:
+                    return true;
+                    //break;
+                case Pawn:
+                    return PawnsTurn(field, coordsFrom, coordsTo);
+                   // break;
+                case Bishop:
+                    return true;
+                   // break;
+                case Rook:
+                    return true;
+                    //break;
+                case Knight:
+                    return true;
+                   // break;
+                case Queen:
+                    return true;
+                    //break;
+            }
+            return true;
+    }
 
-    public TurnResult MakeTurn(PlayerColour colour, Coords coordsFrom, Coords coordsTo) {
+     public Boolean CheckCoords(Coords coords){
+         return coords.X < 0 || coords.X > 7 || coords.Y < 0 || coords.Y > 7; //проверили,что координаты в правильном диапазоне
+     }
+
+    public TurnResult MakeTurn(PlayerColour colour, Coords coordsFrom, Coords coordsTo, ChessField field) {
+         if (CheckCoords(coordsFrom) && !CheckCoords(coordsTo)) return new TurnResult(TurnError.WrongCoords, createGameState());
+         if (!FiguresTurn(field, coordsFrom, coordsTo)) return new TurnResult(TurnError.NotAFigure, createGameState());
         if (colour != Turn)
             return new TurnResult(TurnError.AnotherPlayerTurn, createGameState());
         return new TurnResult(TurnError.None, createGameState());
